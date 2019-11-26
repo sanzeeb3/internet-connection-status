078d744c9d8c7f66e2175dd4dc504b3d961a2da6
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-11-26 13:44:58 +0545

    Introduce themes and it's settings

--- internet-connection-status.php
+++ internet-connection-status.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: Internet Connection Status
  * Description: Automatically alert your users when they've lost internet connectivity
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Sanjeev Aryal
  * Author URI: http://www.sanjeebaryal.com.np
- * Text Domain: internet-connection
+ * Text Domain: internet-connection-status
  *
  * @see  	   https://github.com/HubSpot/offline
  *  
@@ -17,7 +17,7 @@
 
 defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
 
-const ICS_VERSION = '1.0.0';
+const ICS_VERSION = '1.1.0';
 
 /**
  * Enqueue necessary scripts.
@@ -33,3 +33,79 @@ function ics_enqueue_assets() {
 }
 
 add_action( 'wp_enqueue_scripts', 'ics_enqueue_assets' );
+
+/**
+ * Add Internet Connection Status Submenu
+ *
+ * @since  1.1.0
+ */
+function ics_register_setting_menu() {
+	add_options_page( 'Internet Connection Status', 'Internet Connection Status', 'manage_options', 'internet-connection-status', 'ics_settings_page' );
+}
+
+/**
+ * Settings page for Internet Connection Status
+ *
+ * @since 1.1.0
+ */
+function ics_settings_page() {
+	$options = get_option( 'internet_connection_status', array() );
+	$theme 	 = isset( $options['theme'] ) ? $options['theme'] : 'default';
+
+	?>
+		<h2 class="wp-heading-inline"><?php esc_html_e( 'Internet Connection Status Settings', 'internet-connection-status' ); ?></h2>
+		<form method="post">
+			<table class="form-table">
+					<tr valign="top">
+						   <th scope="row"><?php echo esc_html__( 'Theme', 'internet-connection-status' ); ?></th>
+							<td><input type="radio" name="theme" value="default" class="internet-connection-status-theme" <?php checked( $theme, 'default' );?> />
+								<?php echo esc_html__( 'Default', 'internet-connection-status' ); ?>
+
+							</td>
+							<td><input type="radio" name="theme" value="dark" class="internet-connection-status-theme" <?php checked( $theme, 'dark' );?> />
+								<?php echo esc_html__( 'Dark', 'internet-connection-status' ); ?>
+
+							</td>
+							<td><input type="radio" name="theme" value="chrome" class="internet-connection-status-theme" <?php checked( $theme, 'chrome' );?> />
+								<?php echo esc_html__( 'Chrome', 'internet-connection-status' ); ?>
+
+							</td>
+							<td><input type="radio" name="theme" value="indicator-chrome" class="internet-connection-status-theme" <?php checked( $theme, 'indicator-chrome' );?> />
+								<?php echo esc_html__( 'Indicator Chrome', 'internet-connection-status' ); ?>
+
+							</td>
+					</tr>
+					<?php do_action( 'internet_connection_status' ); ?>
+					<?php wp_nonce_field( 'internet_connection_status', 'internet_connection_status_nonce' ); ?>
+
+			</table>
+				<?php submit_button(); ?>
+		</form>
+	<?php
+}
+
+/**
+ * Save Settings.
+ *
+ * @since 1.1.0
+ */
+function ics_save_settings() {
+
+	if ( isset( $_POST['internet_connection_status_nonce'] ) ) {
+		if ( ! wp_verify_nonce( $_POST['internet_connection_status_nonce'], 'internet_connection_status' )
+			) {
+			   print 'Nonce Failed!';
+			   exit;
+		} else {
+			$theme = isset( $_POST['theme'] ) ? sanitize_text_field( $_POST['theme'] ) : '';
+
+			update_option( 'internet_connection_status', array(
+				'theme'  => $theme
+			) );
+
+		}
+	}
+}
+
+add_action( 'admin_menu', 'ics_register_setting_menu' );
+add_action( 'admin_init', 'ics_save_settings' );
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: offline, no-internet, offline-js
 Requires at least: 4.0
 Tested up to: 5.2.3
 Requires PHP: 5.3
-Stable tag: 1.0.0
+Stable tag: 1.1.0
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -12,9 +12,9 @@ Aautomatically alert your users when they've lost internet connectivity
 
 == Description ==
 
-Automatically alert your users when they've lost internet connectivity. The plugin enqueues [Offline.js](https://github.com/HubSpot/offline) by itself.
+Automatically alert your users when they've lost internet connectivity. The plugin enqueues [Offline.js](https://github.com/HubSpot/offline) by itself. It captures AJAX requests which were made while the connection was down, and remakes them when it's back up, so your app reacts perfectly.
 
-Install and activate the plugin. That's it.
+Install and activate the plugin. That's it. Or if you'd like to customize few things, go to Settings > Internet Connection Status.
 
 
 == Frequently Asked Questions ==
