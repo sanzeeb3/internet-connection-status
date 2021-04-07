9479e906948b030c59e8f89942d7e10c6640a423
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-04-07 15:23:52 +0545

    Remove plugin data on uninstallation

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Uninstall WP Frontend Delete Account.
+ *
+ * @since 1.4.3
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+global $wpdb;
+
+// Delete all the Internet Connection Status settings.
+delete_option( 'internet_connection_status' );
+
+// Delete the review notice option.
+delete_option( 'ics_review_notice_dismissed' );
